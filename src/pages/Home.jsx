2a733a8fc2ad08b904f24e5258440ba51ebaf2bd
Jamaal Bernabe <85321486+jnbernabe@users.noreply.github.com>
2025-02-26 16:41:59 -0500

    STMV update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,5 @@
 import React from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  Button,
-  ButtonGroup,
-} from "react-bootstrap";
+import { Container, Row, Col, Card, Button, ButtonGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Resume from "../assets/Jamaal Bernabe - Resume.pdf";
 
@@ -41,13 +34,9 @@ function Home() {
                 </Card.Title>
 
                 <Card.Text className="text-center mx-lg-5">
-                  Software Developer with 3+ years of experience building
-                  scalable, high-performance applications in Python, Java, and
-                  Node.js. Proven expertise in designing RESTful APIs,
-                  optimizing database performance, and deploying cloud-based
-                  solutions on AWS and Azure. Adept at driving automation,
-                  implementing CI/CD pipelines, and leveraging AI-powered
-                  analytics to enhance system efficiency.
+                  Software Developer with 3+ years of experience building scalable, high-performance applications in Python, Java, and Node.js. Proven expertise in designing RESTful APIs, optimizing
+                  database performance, and deploying cloud-based solutions on AWS and Azure. Adept at driving automation, implementing CI/CD pipelines, and leveraging AI-powered analytics to enhance
+                  system efficiency.
                 </Card.Text>
                 <ButtonGroup aria-label="Basic example">
                   <Button variant="primary" href={githubUrl} target="_blank">
@@ -57,12 +46,7 @@ function Home() {
                   <Button variant="info" href={linkedinUrl} target="_blank">
                     LinkedIn
                   </Button>
-                  <Button
-                    variant="success"
-                    href={Resume}
-                    download="Jamaal Bernabe - Resume.pdf"
-                    target="_blank"
-                  >
+                  <Button variant="success" href={Resume} download="Jamaal Bernabe - Resume.pdf" target="_blank">
                     Resume
                   </Button>
                 </ButtonGroup>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,5 @@
-import { Container, Row, Col, Card, Button, CardGroup } from "react-bootstrap";
-import { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Container, Row, Col, Card, Button, CardGroup, Accordion, ButtonGroup } from "react-bootstrap";
+
 import "../App.css";
 
 function Projects() {
@@ -18,49 +17,29 @@ function Projects() {
                   <Card className="text-center bg-primary text-dark">
                     <Card.Header>
                       {" "}
-                      <a
-                        href="https://github.com/jnbernabe/NaviGrade"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-dark"
-                      >
+                      <a href="https://github.com/jnbernabe/NaviGrade" target="_blank" rel="noopener noreferrer" className="text-dark">
                         <b>
                           <h1>NaviGrade </h1>
                         </b>
                       </a>
                     </Card.Header>
+                    <Card.Title>Task and Course Management System</Card.Title>
                     <Card.Body>
                       <Card.Text>
                         {" "}
-                        <b>React</b> | <b>Node.js</b> | <b>MongoDB</b>
-                        <p>
-                          {" "}
-                          A full-stack web application that allows users to
-                          create and manage their own courses and assignments,
-                          as well as view their grades and predict their final
-                          grade.
-                        </p>
-                        <Button
-                          href="https://navigrade-ee7efdf3e932.herokuapp.com/"
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          variant="dark"
-                        >
+                        <b>JavaScript</b> | <b>React</b> | <b>Node.js</b> | <b>MongoDB</b>
+                        <p> A full-stack web application that allows users to create and manage their own courses and assignments, as well as view their grades and predict their final grade.</p>
+                        <Button href="https://navigrade-ee7efdf3e932.herokuapp.com/" target="_blank" rel="noopener noreferrer" variant="dark">
                           Visit NaviGrade on Heroku
                         </Button>
                       </Card.Text>
                     </Card.Body>
                   </Card>
 
-                  <Card className="text-center bg-primary text-dark">
+                  {/* <Card className="text-center bg-primary text-dark">
                     <Card.Header>
                       {" "}
-                      <a
-                        href="https://github.com/jnbernabe/nlp-group1-amzFashion"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-dark"
-                      >
+                      <a href="https://github.com/jnbernabe/nlp-group1-amzFashion" target="_blank" rel="noopener noreferrer" className="text-dark">
                         <b>
                           <h1>Amazon Fashion NLP</h1>
                         </b>
@@ -70,12 +49,37 @@ function Projects() {
                       <Card.Text>
                         {" "}
                         <b>Python</b> | <b>TensorFlow</b> | <b>Scikit-learn</b>
+                        <p> A natural language processing project that analyzes Amazon fashion reviews to predict the sentiment of the review and classify the review's rating.</p>
+                      </Card.Text>
+                    </Card.Body>
+                  </Card> */}
+                  <Card className="text-center bg-primary text-dark">
+                    <Card.Header>
+                      {" "}
+                      <a href="https://github.com/jnbernabe/spotify-to-youtube" target="_blank" rel="noopener noreferrer" className="text-dark">
+                        <b>
+                          <h1>STMV </h1>
+                        </b>
+                      </a>
+                    </Card.Header>
+                    <Card.Title>Playlist to Music Video Generator</Card.Title>
+                    <Card.Body>
+                      <Card.Text>
+                        {" "}
+                        <b>TypeScript</b> | <b>React</b> | <b>Express.js</b> | <b>Node.js</b> | <b>Spotify API</b> | <b>Youtube API</b>
                         <p>
                           {" "}
-                          A natural language processing project that analyzes
-                          Amazon fashion reviews to predict the sentiment of the
-                          review and classify the review's rating.
+                          STMV is a web application that allows users to generate youtube playlists of music videos from their Spotify playlists. The app uses the Spotify API to get the user's
+                          playlists and the Youtube API to generate the music videos. The app uses React to build the UI and Express.js to build the backend. The app is deployed on Netlify.
                         </p>
+                        <ButtonGroup>
+                          <Button href="https://stmv.netlify.app/" target="_blank" rel="noopener noreferrer" variant="dark">
+                            Visit STMV on Netlify
+                          </Button>
+                          <Button href="https://www.youtube.com/watch?v=2f_WMaRrwEo" target="_blank" rel="noopener noreferrer" variant="success">
+                            YouTube Demo
+                          </Button>
+                        </ButtonGroup>
                       </Card.Text>
                     </Card.Body>
                   </Card>
@@ -83,27 +87,21 @@ function Projects() {
                   <Card className="text-center bg-primary text-dark">
                     <Card.Header>
                       {" "}
-                      <a
-                        href="https://github.com/jnbernabe/AWS-Translate-polly"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-dark"
-                      >
+                      <a href="https://github.com/jnbernabe/AWS-Translate-polly" target="_blank" rel="noopener noreferrer" className="text-dark">
                         <b>
-                          <h1>AWS Image Translate</h1>
+                          <h1>Image Translator</h1>
                         </b>
                       </a>
                     </Card.Header>
+                    <Card.Title>AWS Image Translator</Card.Title>
                     <Card.Body>
                       <Card.Text>
                         {" "}
                         <b>Python</b> | <b>AWS Translate</b> | <b>AWS Polly</b>
                         <p>
                           {" "}
-                          A natural language processing project that uses AWS
-                          Translate to translate text in an image from one
-                          language to another and uses AWS Polly to convert the
-                          translated text to speech.
+                          A natural language processing project that uses AWS Translate to translate text in an image from one language to another and uses AWS Polly to convert the translated text to
+                          speech.
                         </p>
                       </Card.Text>
                     </Card.Body>
