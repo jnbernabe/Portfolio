79916bca48803feea1249e7aa14af601ffa772db
Jamaal Bernabe <jnbernabe@gmail.com>
2025-07-27 22:59:41 -0400

    Updated Resume

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Card, Button, ButtonGroup } from "react-bootstrap";
+import { Container, Row, Col, Card, Button, ButtonGroup,ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Resume from "../assets/Jamaal Bernabe - Resume.pdf";
 
@@ -34,9 +34,19 @@ function Home() {
                 </Card.Title>
 
                 <Card.Text className="text-center mx-lg-5">
-                  Software Developer with 3+ years of experience building scalable, high-performance applications in Python, Java, and Node.js. Proven expertise in designing RESTful APIs, optimizing
-                  database performance, and deploying cloud-based solutions on AWS and Azure. Adept at driving automation, implementing CI/CD pipelines, and leveraging AI-powered analytics to enhance
-                  system efficiency.
+                  <ListGroup>
+                                      <ListGroup.Item>•	Over 2 years of experience designing, developing, and testing web and software applications and REST APIs</ListGroup.Item>
+                                      <ListGroup.Item>
+                                        •	Proficient in Java, C#, SQL, and JavaScript and technologies such as .NET, ASP.NET Core, Angular, and React
+                                      </ListGroup.Item><ListGroup.Item>
+                                        •	Experienced in cloud platforms like Microsoft Azure for APIs, databases, and orchestration
+                                      </ListGroup.Item><ListGroup.Item>
+                                        •	Solid understanding of software design patterns, architectural best practices, and performance optimization techniques
+                                      </ListGroup.Item><ListGroup.Item>
+                                        •	Effective verbal and written communication skills for working in Agile software development environments, and collaborating with cross-functional teams
+                                      </ListGroup.Item>
+                  </ListGroup>
+
                 </Card.Text>
                 <ButtonGroup aria-label="Basic example">
                   <Button variant="primary" href={githubUrl} target="_blank">
